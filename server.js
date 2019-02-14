7eb77721aac268aed0cb2ce2d28c01ab55e4b371
Ajay Gandhi <aag255@cornell.edu>
2019-02-14 13:06:59 -0800

    Fix next muni

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 const express = require("express");
 const https = require("https");
+const http = require("http");
+const parseString = require("xml2js").parseString;
 const priv = require("./private");
 
 const app = express();
@@ -8,17 +10,13 @@ const app = express();
 app.use(express.static("public"));
 
 app.get("/weather", (req, res) => {
-  https.get(`https://api.darksky.net/forecast/${priv.weatherApiKey}/37.735988,-122.388798`, (response) => {
-    let data = "";
-    response.on("data", c => data += c);
-    response.on("end", () => {
-      res.send(JSON.parse(data).daily.data[0]);
-    });
+  request(`https://api.darksky.net/forecast/${priv.weatherApiKey}/37.735988,-122.388798`, (data) => {
+    res.send(JSON.parse(data).daily.data[0]);
   });
 });
 
 app.get("/muni", (req, res) => {
-  https.get(`http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=sf-muni&stopId=${priv.muniStop}`, (response) => {
+  request(`http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=${priv.muniAgency}&r=${priv.muniRoute}&s=${priv.muniStopId}`, (response) => {
     parseString(response, (err, result) => {
       if (err) {
         return res.send(JSON.stringify({ error: err }));
@@ -34,9 +32,21 @@ app.get("/muni", (req, res) => {
         else if (f < memo[1]) return [memo[0], f];
         else                  return memo;
       }, [Number.MAX_SAFE_INTEGER, Number.MAX_SAFE_INTEGER]);
-      res.send({ next: closestTrains });
+      res.send({ closestTrains });
     });
   });
 });
 
 app.listen(8000, () => console.log("Listening on 8000"));
+
+// http request helper
+const request = (url, cb) => {
+  const prot = url.startsWith("https") ? https : http;
+  prot.get(url, (response) => {
+    let data = "";
+    response.on("data", c => data += c);
+    response.on("end", () => {
+      cb(data);
+    });
+  });
+};
